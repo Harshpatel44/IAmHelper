b48d5493960400c351b03b57eb39d4133291ac7a
harshPatel44 <harshgp44@gmail.com>
2018-01-28 13:57:48 +0530

    dragging done

--- api_calling.py
+++ api_calling.py
@@ -4,37 +4,19 @@ import json
 
 
 def get_response(word_id):
-    # language='en'
-    # #word_id='harsh'
-    # app_id = '433bec40'
-    # app_key = '90d6bd1e273afb373e23e082604d6d8d'
-    #
-    # url='https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()+'/synonyms'
-    # r=requests.get(url,headers={'app_id':app_id,'app_key':app_key})
-    # string=r.content
-    # result=re.findall("\"synonyms\": .*\W.*\W.*\"id\":\W\"(.*)\"",string)
-
-    try:
+        try:
         print(word_id)
         url="""http://api.wordnik.com/v4/word.json/"""+str(word_id).lower()+"""/definitions?limit=200&includeRelated=true&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"""
         r=requests.get(url)
         string=r.content
-        #print(string)
-        j=json.loads(string)
-        #print(j[0])
-        #for i in j[0:5]:
-            #print(i['text'])
-        #print(j[1]['text'])
+        j=json.loads(string)    #loading the content in json format
         list=[]
         list.extend([j[0]['text'],j[1]['text'],j[2]['text'],j[3]['text'],j[4]['text']])
         #print(list)
-        return list
+        return list    #returning 5 values in list
 
     except:
         return ["No Result"]
 
 
-#harsh
 
-#list=list(a)
-#print(list)
--- findMeaning.py
+++ findMeaning.py
@@ -8,7 +8,7 @@ import urllib
 
 
 def start_module():
-    state_left=win32api.GetAsyncKeyState(0x12)
+    state_left=win32api.GetAsyncKeyState(0x12)     #get the key state
     while True:
         a=win32api.GetAsyncKeyState(0x12)
         if (a != state_left):
@@ -25,21 +25,36 @@ def start_module():
 
                 main=tk.Tk()              #starting Tkinter window
                 main.configure(background="#8cb3d9")
-                var=tk.StringVar()        #string variable
+                var=tk.StringVar()  #string variable
+
                 main.overrideredirect(True)
                 main.wm_attributes('-alpha',0.5)
                 main.wm_attributes('-topmost',True)
                 data=win32api.GetCursorPos()
                 x,y=data
-
                 main.geometry("+%d+%d"%(x-30,y+10))
+                def start(event,self):                  #dragging the window
+                    self.x = event.x 
+                    self.y = event.y
+                def motion(event,self):
+                    deltax = event.x - self.x
+                    deltay = event.y - self.y
+                    x = self.winfo_x() + deltax
+                    y = self.winfo_y() + deltay
+                    self.geometry("+%s+%s" % (x, y))
+
+                def StopMove(self, event):
+                    self.x = None
+                    self.y = None
+                main.bind('<ButtonPress-1>',lambda event,arg=main:start(event,arg))
+                main.bind('<B1-Motion>',lambda event,arg=main:motion(event,arg))
                 global response
                 response=[]
 
 
                 def check_length(response):
-                    
-                    if(len(response)>=100):                      #if length is more than displaying in more lines
+
+                    if(len(response)>=100):                      #if length is more, than displaying in more lines
                         value=int(len(response)/100)
                         response_edit=""
                         for i in range(1,value+2):       #iterating with adding \n at every 100 characters
@@ -51,21 +66,22 @@ def start_module():
                         var.set(response)
 
 
-                def api_call(a):
+                def api_call(a):    #gets the response from the api
                     global response
                     response=api_calling.get_response(a)
 
                     check_length(response[0])
 
-
-
+                # heading=tk.Canvas(width=100,height=10,background="#333333")
+                # heading.pack()
+                # label=tk.Label(heading,text=a)
+                # label.pack(expand='true',fill='both')
                 canvas=tk.Canvas(main,height=10,width=600)           #title Bar
                 canvas.pack(side='left')
-                #canvas.bind('<ButtonPress-1>',lambda event,arg=main:start(event,arg))
-                #canvas.bind('<B1-Motion>',lambda event,arg=main:motion(event,arg))
+
                 def close(event):
                     main.destroy()
-
+                #layout creationn
                 btn=tk.Button(canvas,text='X',relief=tk.GROOVE,font=('Lithos Pro Regular',8),fg="#bbbbbb",width=7,anchor=tk.CENTER,background='#333333')
                 btn.bind('<Button-1>',close)
                 btn.pack(side='left')
@@ -86,7 +102,6 @@ def start_module():
                     if(flag<4):
                         flag+=1
                         check_length(response[flag])
-
                 def browsing(event):
                     webbrowser.open('https://www.google.co.in/search?dcr=0&source=hp&ei=pulsWuDXF4OSvQTtza64CA&q='+urllib.quote(a)+'+&oq='+urllib.quote(a)+'+&gs_l=psy-ab.3..0l10.5783.9141.0.9586.25.14.4.2.2.0.307.2266.0j1j7j1.9.0....0...1c.1.64.psy-ab..10.15.2286.0..0i67k1j0i131k1.0.teo2iQHj36g')
                     close(event)
@@ -95,7 +110,7 @@ def start_module():
                     close(event)
 
 
-                more_btn.bind("<Button-1>",lambda event:more(event))
+                more_btn.bind("<Button-1>",lambda event:more(event))   #bindings for all the buttons
                 search_btn.bind('<Button-1>',browsing)
                 youtube_btn.bind("<Button-1>",youtube)
                 _thread.start_new_thread(api_call,(a,))
@@ -105,6 +120,7 @@ def start_module():
 
                 def opaque(event):
                     main.wm_attributes('-alpha',1)
+
                 def tspnt(event):
                     main.wm_attributes('-alpha',0.5)
                 main.bind("<Enter>",opaque)
